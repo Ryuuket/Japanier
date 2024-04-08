d8e968f1468819b7b0dd7f73f31e756fc21bae37
Axel Galvier <axelgalvier0@gmail.com>
2024-04-08 09:27:53 +0200

    Initialized business part of the project.

---
+++ japanier-business/src/main/java/co/simplon/japanier/japanierbusiness/JapanierBusinessApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.japanier.japanierbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanierBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanierBusinessApplication.class, args);
+	}
+
+}
