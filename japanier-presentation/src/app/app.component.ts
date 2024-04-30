08cd2d3397ee6b9017344f8940a46b236c5484b8
Axel Galvier <axelgalvier0@gmail.com>
2024-04-30 09:56:55 +0200

    Replaced Authentication by Registration.

--- japanier-presentation/src/app/app.component.ts
+++ japanier-presentation/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AuthenticationComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- japanier-presentation/src/app/app.routes.ts
+++ japanier-presentation/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { AuthenticationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
