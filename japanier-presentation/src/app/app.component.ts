1b1183cba78a1f602ed48b9430776371f2e74bed
Axel Galvier <axelgalvier0@gmail.com>
2024-04-30 09:39:38 +0200

    Added Home page and some routing.

--- japanier-presentation/src/app/app.component.ts
+++ japanier-presentation/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AuthenticationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
