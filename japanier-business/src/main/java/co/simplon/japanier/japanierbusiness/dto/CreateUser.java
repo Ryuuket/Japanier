f29274b4a4a6f0bdc0a0de3959072aa4b982186f
Axel Galvier <axelgalvier0@gmail.com>
2024-05-16 13:34:33 +0200

    Created User DTOs.

--- japanier-business/src/main/java/co/simplon/japanier/japanierbusiness/dto/CreateUser.java
+++ japanier-business/src/main/java/co/simplon/japanier/japanierbusiness/dto/CreateUser.java
@@ -1,21 +1,31 @@
 package co.simplon.japanier.japanierbusiness.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public class CreateUserDTO {
-
-    @NotNull
-    @Email
+public class CreateUser {
+    @Pattern(regexp = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$", message = "L'adresse mail doit être valide.")
     private String email;
 
-    @NotNull
-    @Pattern(regexp = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$", message = "Le mot de passe contient au moins 8 caractères, dont une majuscule, une minuscule et un caractère spécial.")
     private String password;
 
-    public CreateUserDTO() {
+    public CreateUser() {
 
     }
 
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
 }
