8ce7be06368a6513f0fc2794457384315f4a8000
Ryuuket <144806364+Ryuuket@users.noreply.github.com>
2024-05-29 16:03:49 +0200

    Changed projet's name in frontend.

--- japanier-presentation/src/app/app.component.ts
+++ japanier-presentation/src/app/app.component.ts
@@ -5,14 +5,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { LessonsListComponent } from './lessons-list/lessons-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, RegistrationComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, RegistrationComponent, LoginComponent, LessonsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-starter';
+  title = 'japanier';
 }
